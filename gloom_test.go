a4743ced8ca13b88f7d37073ade5789def666af7
xngln <davxliu@gmail.com>
2020-05-30 22:31:40 -0400

    Add test cases for Add func

--- gloom_test.go
+++ gloom_test.go
@@ -15,3 +15,19 @@ func TestNew(t *testing.T) {
 		t.Errorf("Got: %v, expected: %v", bf.k, k)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	m := uint64(100)
+	k := uint64(6)
+	bf := New(m, k)
+
+	bf.AddString("golang")
+
+	if !bf.ContainsString("golang") {
+		t.Errorf("Got false, expected true")
+	}
+
+	if bf.ContainsString("python") {
+		t.Errorf("Got true, expected false")
+	}
+}
